beee96b2349846be64eab69387a2a20207cecc03
mimichen <cs.liuchen@gmail.com>
2020-04-20 20:10:49 +0800

    1

--- mi_store/src/App.vue
+++ mi_store/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <router-view></route-view>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
